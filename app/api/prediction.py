550988abc46adc82294e1164c65d2fcc3638f117
F. Waskito <fajar.waskito10@gmail.com>
2024-07-08 05:29:56 +0700

    Add app/api/prediction.py

---
+++ app/api/prediction.py
@@ -0,0 +1,82 @@
+from flask import request, session, render_template, flash
+import pandas as pd
+from psycopg2.extras import RealDictCursor
+from app.models.database import Database as db
+from app.utils.classification import Classifier
+from app.api import bp
+
+@bp.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    query = 'get_test_data'
+    conn = db().get_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.callproc(query)
+    test_data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    if session.get('predictions') == None:
+        predictions = {}
+        session['predictions'] = predictions
+
+    if request.method == 'POST':
+        # Get train data
+        query = 'get_train_data'
+        conn = db().get_connection()
+        cursor = conn.cursor()
+        cursor.callproc(query)
+        train_df = pd.DataFrame(cursor.fetchall())
+
+        if request.form.get('predict_type') == 'single':
+            # Get test data
+            _id = request.form.get('id')
+
+            for data in test_data:
+                if data['id'] == _id:
+                    single_test_data = [data]
+                    break
+            test_df = pd.DataFrame(single_test_data)
+
+            # Separating train and test data
+            train_df.drop('employee_id', axis=1, inplace=True)
+            test_id = test_df['id'].values.tolist()
+            test_df.drop('id', axis=1, inplace=True)
+
+            # Attrition classification using k-NN with vluew k=7
+            cls = Classifier()
+            predictions = cls.get_classification(train_df, test_df, test_id, k=7)
+
+            predictions_sess = session.get('predictions')
+            predictions_sess[_id] = predictions[_id]
+            session['predictions'] = predictions_sess
+            flash(str(len(test_id)) + " data sucessfully predicted.")
+
+            return render_template('prediction.html', test_data=test_data)
+
+        predictions_sess = session.get('predictions')
+        predicted_id = predictions_sess.keys()
+
+        many_test_data = []
+        for data in test_data:
+            if data['id'] not in predicted_id:
+                many_test_data.append(data)
+        test_df = pd.DataFrame(many_test_data)
+
+        # Separatin the indetifier of train and test data
+        train_df.drop('employee_id', axis=1, inplace=True)
+        test_id = test_df['id'].values.tolist()
+        test_df.drop('id', axis=1, inplace=True)
+
+        # Attrition prediction using k-NN with value k=7
+        cls = Classifier()
+        predictions = cls.get_classification(train_df, test_df, test_id, k=7)
+
+        for id in test_id:
+            predictions_sess[id] = predictions[id]
+
+        session['predictions'] = predictions_sess
+        flash(str(len(test_id)) + " data sucessfully predicted.")
+        return render_template('prediction.html', test_data = test_data)
+
+    return render_template('prediction.html', test_data = test_data)
