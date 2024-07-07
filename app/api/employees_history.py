78031b8e42d882f2bc2bd0b787cf663179ae9b3f
F. Waskito <fajar.waskito10@gmail.com>
2024-07-08 04:25:10 +0700

    Add app/api/employees_history.py

---
+++ app/api/employees_history.py
@@ -0,0 +1,28 @@
+from flask import session, render_template
+from app.models.database import Database as db
+from psycopg2.extras import RealDictCursor
+from app.api import bp
+
+@bp.route('/history', methods=['GET', 'POST'])
+def employees_history():
+    if session.get('user'):
+        query = 'get_train_data_distribution'
+        conn = db().get_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        cursor.callproc(query)
+        class_distrib = cursor.fetchall()[0]
+
+        query = 'get_employees_history'
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        cursor.callproc(query)
+        employees_history_data = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+        return render_template(
+            'history.html',
+            employees_history = employees_history_data,
+            class_distrib = class_distrib,
+        )
+
+    return render_template('error.html', error='Unauthorized Access')
