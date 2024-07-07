9314512df376c05feb53f0d30b3f1df770bfaa80
F. Waskito <fajar.waskito10@gmail.com>
2024-07-07 22:53:45 +0700

    Add config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ['SECRET_KEY']
+
+class DatabaseConfig:
+    HOST = os.environ["POSTGRES_HOST"]
+    DATABASE = os.environ["POSTGRES_DATABASE"]
+    USER = os.environ['POSTGRES_USER']
+    PASSWORD = os.environ['POSTGRES_PASSWORD']
