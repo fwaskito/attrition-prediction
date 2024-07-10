094752cf3035b91112a4b82a6a6c2089cb66f836
F. Waskito <fajar.waskito10@gmail.com>
2024-07-10 22:58:32 +0700

    Update app/api/main.py: pass the page variable

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app.api import bp
 
-@bp.route('/')
+
+@bp.route("/")
 def main():
-    return render_template('index.html')
+    return render_template("index.html", page="main")
