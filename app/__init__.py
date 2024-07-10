0b0f7b72a5e44e0c5e9db4aef9c5f535c3452c6f
F. Waskito <fajar.waskito10@gmail.com>
2024-07-10 22:40:51 +0700

    Update app/__ini__.py: change test route page content

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from app.api import bp as api_blueprint
 
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
@@ -9,8 +10,8 @@ def create_app(config_class=Config):
     # Register blueprints
     app.register_blueprint(api_blueprint)
 
-    @app.route('/test/')
+    @app.route("/test/")
     def test_page():
-        return '<h1>Testing the Flask Application Factory</h1>'
+        return "<h2>Testing the Attrition Prediction System</h2>"
 
     return app
