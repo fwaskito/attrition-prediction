819151e8e29ee2f95dc59d7843951f10e0e10c3b
F. Waskito <fajar.waskito10@gmail.com>
2024-07-10 23:28:35 +0700

    Add page variable to render in app/api/authentication.py

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -4,39 +4,42 @@ from app.models.database import Database
 from app.forms import SigninForm
 from app.api import bp
 
-@bp.route('/login', methods=['GET', 'POST'])
+
+@bp.route("/login", methods=["GET", "POST"])
 def login():
     form = SigninForm()
     if form.validate_on_submit():
         try:
-            _username = form.username.data
-            _password = form.password.data
+            username = form.username.data
+            password = form.password.data
 
-            query = "select * from sppk.user where username='"+_username + "';"
+            query = "select * from sppk.user where username='" + username + "';"
             conn = Database().get_connection()
             cursor = conn.cursor(cursor_factory=RealDictCursor)
             cursor.execute(query)
             data = cursor.fetchall()
 
             if len(data) > 0:
-                if str(data[0]['password']) == str(_password):
-                    session['user'] = data[0]
-                    return redirect(url_for('api.employees'))
+                if str(data[0]["password"]) == str(password):
+                    session["user"] = data[0]
+                    return redirect(url_for("api.employees"))
                 else:
-                    flash("Password not valid!")
-                    return render_template('login.html', form=form)
+                    flash("Password invalid!")
+                    return render_template("login.html", form=form, page="login")
             else:
-                flash("Username not valid!")
-                return render_template('login.html', form=form)
+                flash("Username invalid!")
+                return render_template("login.html", form=form, page="login")
 
         except Exception as e:
             print("Execption: ", e)
-            return render_template('error.html', error=str(e))
+            return render_template("error.html", error=str(e))
 
-    return render_template('login.html', form=form)
+    return render_template("login.html", form=form, page="login")
 
-@bp.route('/logout')
+
+@bp.route("/logout")
 def logout():
-    session.pop('user', None)
-    session.pop('predictions', None)
-    return redirect(url_for('api.main'))
+    session.pop("user", None)
+    session.pop("predictions", None)
+    session.pop("new_id", None)
+    return redirect(url_for("api.main"))
