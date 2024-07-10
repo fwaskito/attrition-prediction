bcfb28199b60f20458370198b48eadc1ec6cb3b2
F. Waskito <fajar.waskito10@gmail.com>
2024-07-10 23:37:52 +0700

    Improve config.py code formatting

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ["SECRET_KEY"]
+
 
 class DatabaseConfig:
     HOST = os.environ["POSTGRES_HOST"]
     DATABASE = os.environ["POSTGRES_DATABASE"]
-    USER = os.environ['POSTGRES_USER']
-    PASSWORD = os.environ['POSTGRES_PASSWORD']
+    USER = os.environ["POSTGRES_USER"]
+    PASSWORD = os.environ["POSTGRES_PASSWORD"]
