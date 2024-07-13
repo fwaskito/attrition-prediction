8996ebf1812704881c6645047856994cca130742
F. Waskito <fajar.waskito10@gmail.com>
2024-07-13 21:24:48 +0700

    refactor: update import module names to employee and employee_history

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,6 +4,6 @@ bp = Blueprint("api", __name__)
 
 from app.api import main
 from app.api import authentication
-from app.api import employees
-from app.api import employees_history
+from app.api import employee
+from app.api import employee_history
 from app.api import prediction
