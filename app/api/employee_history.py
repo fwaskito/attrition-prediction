76be426b675d780529dfa093fec127999da3acd3
F. Waskito <fajar.waskito10@gmail.com>
2024-07-14 08:31:26 +0700

    perf: provide better training data by change the body of get_train_data() function

--- app/api/employee_history.py
+++ app/api/employee_history.py
@@ -7,23 +7,38 @@ from app.api import bp
 @bp.route("/employee-histories", methods=["GET"])
 def employee_histories():
     if session.get("user"):
-        query = "get_train_data_distribution"
         conn = db().get_connection()
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        cursor.callproc(query)
-        class_distrib = cursor.fetchall()[0]
-
         query = "get_employees_history"
         cursor = conn.cursor(cursor_factory=RealDictCursor)
         cursor.callproc(query)
-        employees_history_data = cursor.fetchall()
+        employee_histories_data = cursor.fetchall()
+
+        query = "get_train_class_distribution"
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        cursor.callproc(query)
+        train_class_distrib = cursor.fetchall()[0]
 
         cursor.close()
         conn.close()
 
+        if train_class_distrib["e_attrition_no"] == None:
+            train_class_distrib["e_attrition_no"] = 0
+        if train_class_distrib["eh_attrition_no"] == None:
+            train_class_distrib["eh_attrition_no"] = 0
+        if train_class_distrib["eh_attrition_yes"] == None:
+            train_class_distrib["eh_attrition_yes"] = 0
+
+        class_distrib = {
+            "no": (
+                train_class_distrib["e_attrition_no"]
+                + train_class_distrib["eh_attrition_no"]
+            ),
+            "yes": train_class_distrib["eh_attrition_yes"],
+        }
+
         return render_template(
             "history.html",
-            employees_history=employees_history_data,
+            employee_histories=employee_histories_data,
             class_distrib=class_distrib,
             page="history",
         )
