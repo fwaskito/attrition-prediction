ef9f175ade512f05939ac3d6f69137a81bc01570
F. Waskito <fajar.waskito10@gmail.com>
2024-07-18 03:14:52 +0700

    style: add page elements fade-in animation

--- app/static/assets/js/display.js
+++ app/static/assets/js/display.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $("#messageModal").modal('show');
+	setTimeout(function() {
+		$('#messageModal').modal('show');
+	}, 1000);
+});
+
+$(window).on("load", function() {
+	$('.home-page-content').fadeIn(2000);
+	$('.history-page-content').fadeIn(1500);
+	$('.prediction-page-content').fadeIn(1000);
 });
