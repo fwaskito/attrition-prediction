e31270436e5af32f62402d558f5b9b01aebf3d77
F. Waskito <fajar.waskito10@gmail.com>
2024-07-18 19:25:00 +0700

    style: add fede-in animation for main and login pages

--- app/static/assets/js/display.js
+++ app/static/assets/js/display.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 });
 
 $(window).on("load", function() {
+	$('.main-page-content').fadeIn(1000);
+	$('.login-page-content').fadeIn(1000);
 	$('.home-page-content').fadeIn(2000);
 	$('.history-page-content').fadeIn(1500);
 	$('.prediction-page-content').fadeIn(1000);
