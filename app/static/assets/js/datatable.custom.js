8e43526e6cf716dbea03cdb40ee86fc9dcee731b
F. Waskito <fajar.waskito10@gmail.com>
2024-07-19 14:56:18 +0700

    feat: add expand all and collapse all child-rows click handlers

--- app/static/assets/js/datatable.custom.js
+++ app/static/assets/js/datatable.custom.js
@@ -1,7 +1,17 @@
 $(document).ready(function (){
-	var table = $('#example').DataTable({
+	var table = $('#dataTable').DataTable({
 		dom: 'Bfrtip',
 		buttons: ['csv', 'excel', 'pdf'],
 		responsive: true
+	});
+
+	// Handle click on "Expand All" button
+	$('#btn-show-all-children').on('click', function(){
+		table.rows(':not(.parent)').nodes().to$().find('td:first-child').trigger('click');
+	});
+
+	// Handle click on "Collapse All" button
+	$('#btn-hide-all-children').on('click', function(){
+		table.rows('.parent').nodes().to$().find('td:first-child').trigger('click');
 	});
 });
