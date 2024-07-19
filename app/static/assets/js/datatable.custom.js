5a0707a6e8c7d4198511c96c465de84aa1d692df
F. Waskito <fajar.waskito10@gmail.com>
2024-07-19 17:26:04 +0700

    fix: add branching to resolve mismatched column counts when exporting employee history data

--- app/static/assets/js/datatable.custom.js
+++ app/static/assets/js/datatable.custom.js
@@ -1,32 +1,47 @@
 $(document).ready(function (){
-	var employeeCols = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
-
-	var table = $('#dataTable').DataTable({
-		dom: 'Bfrtip',
-		buttons : [
-			{
-				extend: 'csvHtml5',
-				exportOptions: {
-					columns: employeeCols
+	if ($('#dataTable').length) {
+		var table = $('#dataTable').DataTable({
+			dom: 'Bfrtip',
+			buttons : [
+				'csv',
+				'excel',
+				{
+					extend: 'pdfHtml5',
+					orientation: 'landscape',
+					pageSize: 'LEGAL'
 				}
-			},
-			{
-				extend: 'excelHtml5',
-				exportOptions: {
-					columns: employeeCols
+			],
+			responsive: true
+		});
+	} else {
+		var employeeCols = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+		var table = $('#employeesDataTable').DataTable({
+			dom: 'Bfrtip',
+			buttons : [
+				{
+					extend: 'csvHtml5',
+					exportOptions: {
+						columns: employeeCols
+					}
+				},
+				{
+					extend: 'excelHtml5',
+					exportOptions: {
+						columns: employeeCols
+					}
+				},
+				{
+					extend: 'pdfHtml5',
+					orientation: 'landscape',
+					pageSize: 'LEGAL',
+					exportOptions: {
+						columns: employeeCols
+					}
 				}
-			},
-			{
-				extend: 'pdfHtml5',
-				orientation: 'landscape',
-				pageSize: 'LEGAL',
-				exportOptions: {
-					columns: employeeCols
-				}
-			}
-		],
-		responsive: true
-	});
+			],
+			responsive: true
+		});
+	}
 
 	// Handle click on "Expand All" button
 	$('#btn-show-all-children').on('click', function(){
